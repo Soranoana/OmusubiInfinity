e52e8afdba60a5f6c845135e03edbe65660f7715
ソラノアナ Soranoana <Inanis_foramen@yahoo.co.jp>
2023-03-29 03:01:42 +0900

    ChatGPTにリファクタリングさせた。すこし修正

--- Assets/UserControl.cs
+++ Assets/UserControl.cs
@@ -1,19 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Rendering;
 
 public class UserControl : MonoBehaviour {
-
-	public GameObject omusubiObject;
-	public GameObject omusubiStartPosition;
+	[SerializeField] private GameObject omusubiObject;
+	[SerializeField] private GameObject omusubiStartPosition;
 	private FieldInfo fieldInfo;
+	[SerializeField] private Text text1;
+	[SerializeField] private Text text2;
 
-	public GameObject text1;
-	public GameObject text2;
-
-	private int flame = -1;
+	private int frame = -1;
 	private float?[] range = new float?[60];
 
 	void Start() {
@@ -25,54 +22,56 @@ public class UserControl : MonoBehaviour {
 
 	void Update() {
 		if (Input.GetMouseButton(0)) {
+			MoveOmusubi();
+		}
+		SpeedManage();
+	}
 
+	private void MoveOmusubi() {
+		if (Input.GetMouseButton(0)) {
 			float omusubiX = omusubiObject.transform.position.x;
 			float omusubiStartX = omusubiStartPosition.transform.position.x;
 			float courseWidth = fieldInfo.courseWidth;
-			float MouseX = Input.GetAxis("Mouse X");
-
-			if (omusubiX + MouseX >= omusubiStartX + courseWidth) {
+			float mouseX = Input.GetAxis("Mouse X");
+			if (omusubiX + mouseX >= omusubiStartX + courseWidth) {
 				omusubiObject.transform.position = new Vector3(omusubiStartX + courseWidth, omusubiObject.transform.position.y, omusubiObject.transform.position.z);
-			} else if (omusubiX + MouseX <= omusubiStartX - courseWidth) {
+			} else if (omusubiX + mouseX <= omusubiStartX - courseWidth) {
 				omusubiObject.transform.position = new Vector3(omusubiStartX - courseWidth, omusubiObject.transform.position.y, omusubiObject.transform.position.z);
 			} else {
-				omusubiObject.transform.position += new Vector3(MouseX, 0, 0);
+				omusubiObject.transform.position += new Vector3(mouseX, 0, 0);
 			}
 		}
-		SpeedManege();
-
 	}
 
-	void SpeedManege() {
+	private void SpeedManage() {
 		SpeedTest1();
 		SpeedTest2();
 	}
 
-	void SpeedTest1() {
-		//ベクトル量から算出
-		Vector3 velocityLocal = new Vector3(0, omusubiObject.GetComponent<Rigidbody>().velocity.y, omusubiObject.GetComponent<Rigidbody>().velocity.z);
-		string text = "speedTest1: " + Vector3.Distance(velocityLocal, Vector3.zero);
-		// Debug.Log(text);
+	//ベクトル量から算出
+	private void SpeedTest1() {
+		Vector3 velocityLocal = omusubiObject.GetComponent<Rigidbody>().velocity;
+		velocityLocal.x = 0f;
+		float speed = velocityLocal.magnitude;
+		string text = "speedTest1: " + speed;
 		text1.GetComponent<Text>().text = text;
 	}
 
-	void SpeedTest2() {
-		//移動距離から算出
-		Vector3 omusubiPositionLocal = new Vector3(0, omusubiObject.transform.position.y, omusubiObject.transform.position.z);
-		string text;
-		flame++;
+	//移動距離から算出
+	private void SpeedTest2() {
+		frame++;
+		Vector3 omusubiPositionLocal = omusubiObject.transform.position;
+		omusubiPositionLocal.x = 0f;
 		float rangeLocal = Vector3.Distance(omusubiStartPosition.transform.position, omusubiPositionLocal);
-		if (range[flame % 60] == null) {
-			text = "speedTest2: " + ( rangeLocal / flame * 10000 );
-			// Debug.Log(text);
+		if (range[frame % 60] == null) {
+			float speed = rangeLocal / frame * 10000;
+			string text = "speedTest2: " + speed;
 			text2.GetComponent<Text>().text = text;
 		} else {
-			text = "speedTest2: " + ( ( rangeLocal - range[flame % 60] ) / 60 * 10000 );
-			// Debug.Log(text);
+			float speed = ( rangeLocal - range[frame % 60].Value ) / 60f * 10000f;
+			string text = "speedTest2: " + speed;
 			text2.GetComponent<Text>().text = text;
 		}
-
-		range[flame % 60] = rangeLocal;
+		range[frame % 60] = rangeLocal;
 	}
-
 }
